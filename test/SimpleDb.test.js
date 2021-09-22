64ae39df7e12b2e8d35bb3752378a3a02720a9d6
satyadvaya <satyadvaya@gmail.com>
2021-09-22 00:34:50 -0700

    failing test on get all objects

--- test/SimpleDb.test.js
+++ test/SimpleDb.test.js
@@ -2,38 +2,52 @@ import { rm, mkdir } from 'fs/promises';
 import { SimpleDb } from '../SimpleDb.js';
 
 describe('SimpleDb', () => {
-  const rootDir = './store';
+    const rootDir = './store';
 
-  beforeEach(() => {
-    return rm(rootDir, { recursive: true, force: true }).then(() => {
-      return mkdir(rootDir, { recursive: true });
+    beforeEach(() => {
+        return rm(rootDir, { recursive: true, force: true }).then(() => {
+            return mkdir(rootDir, { recursive: true });
+        });
     });
-  });
 
-  it('should create a file within RootDir', async () => {
-    const newSimpleDb = new SimpleDb(rootDir);
-    const objectContent = { word: 'banana' };
+    it('should create a file within RootDir', async () => {
+        const newSimpleDb = new SimpleDb(rootDir);
+        const objectContent = { word: 'banana' };
 
-    await newSimpleDb.save(objectContent);
+        await newSimpleDb.save(objectContent);
 
-    expect(objectContent.id).toEqual(expect.any(String));
-  });
+        expect(objectContent.id).toEqual(expect.any(String));
+    });
 
-  it('should save and get an object', async () => {
-    const newSimpleDb = new SimpleDb(rootDir);
-    const objectContent = { word: 'banana' };
+    it('should save and get an object', async () => {
+        const newSimpleDb = new SimpleDb(rootDir);
+        const objectContent = { word: 'banana' };
 
-    await newSimpleDb.save(objectContent);
-    const objectId = await newSimpleDb.get(objectContent.id);
+        await newSimpleDb.save(objectContent);
+        const objectId = await newSimpleDb.get(objectContent.id);
 
-    expect(objectId).toEqual(objectContent);
-  });
+        expect(objectId).toEqual(objectContent);
+    });
 
-  it('should return null for nonexistent id', async () => {
-    const newSimpleDb = new SimpleDb(rootDir);
+    it('should return null for nonexistent id', async () => {
+        const newSimpleDb = new SimpleDb(rootDir);
 
-    const objectId = await newSimpleDb.get('nonexistent-id');
+        const objectId = await newSimpleDb.get('nonexistent-id');
 
-    expect(objectId).toBe(null);
-  });
+        expect(objectId).toBe(null);
+    });
+
+    it('should get all objects', async () => {
+        const newSimpleDb = new SimpleDb(rootDir);
+        const fruits = [
+            { word: 'banana' },
+            { word: 'plum' },
+            { word: 'cherry' },
+        ];
+
+        await Promise.all(fruits.map((fruit) => newSimpleDb.save(fruit)));
+        const objectId = await newSimpleDb.getAll();
+
+        expect(objectId).toEqual(expect.arrayContaining(fruits));
+    });
 });
